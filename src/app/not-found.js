edfb0afec159c181aad7cfc84a725ace84d760ca
magytar <rafal2oko.3453@gmail.com>
2025-06-03 04:40:35 -0300

    vamo la

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+// app/not-found.js
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: "center", padding: "100px" }}>
+      <h1>404 - Página não encontrada</h1>
+      <p>Essa rota não existe no app seu maconheiro.</p>
+    </div>
+  );
+}
